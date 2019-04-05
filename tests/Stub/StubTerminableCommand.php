e31ee35df556b58c40765fa04f5231ac7a9d5ce7
Alessandro Lai <alessandro.lai85@gmail.com>
2019-04-05 16:27:24 +0200

    Add stub terminable command

---
+++ tests/Stub/StubTerminableCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Facile\TerminableLoop\Tests\Stub;
+
+use Facile\TerminableLoop\AbstractTerminableCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Process;
+
+class StubTerminableCommand extends AbstractTerminableCommand
+{
+    public function __construct()
+    {
+        parent::__construct('stub:terminable:sleep');
+    }
+
+    public function configure(): void
+    {
+        $this->addOption('sleep', null, InputOption::VALUE_REQUIRED, 'Sleep duration time', 0);
+    }
+
+    protected function commandBody(InputInterface $input, OutputInterface $output): int
+    {
+        $sleepDuration = $this->getIntArgument($input, 'sleep');
+
+        if ($sleepDuration > 0) {
+            $output->writeln(sprintf('Sleeping %d seconds', $sleepDuration));
+
+            $this->setSleepDuration($sleepDuration);
+
+            $process = new Process(['sleep', $sleepDuration]);
+            $process->run();
+
+            $output->writeln('Elaborazione terminata');
+        }
+
+        return 1; //force exit from bash
+    }
+
+    /**
+     * @throws \InvalidArgumentException
+     */
+    protected function getIntArgument(InputInterface $input, string $paramName): int
+    {
+        $paramValue = $input->getArgument($paramName);
+
+        if (is_numeric($paramValue)) {
+            $value = (int) $paramValue;
+
+            if ($value < 0) {
+                throw new \InvalidArgumentException($paramName . ': Not a positive integer value');
+            }
+
+            return $value;
+        }
+
+        throw new \InvalidArgumentException(
+            $paramName . ': Can\'t return an int from ' . print_r($paramValue, true)
+        );
+    }
+}
